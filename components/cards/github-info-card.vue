ce261a532ddcabd55a7e3fe95c3865ca831688e8
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-05-12 12:37:25 +0200

    feat: implement info cards for spotify and login

---
+++ components/cards/github-info-card.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card max-width="50rem" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          Connected
+        </v-list-item-title>
+        <v-list-item-subtitle>Logged in as {{user.username}}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <!-- <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar> -->
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn class="ml-auto" text @click="logout">
+        Logout
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import auth from '~/mixins/auth'
+import device from '~/mixins/device'
+
+export default {
+  name: 'GithubInfoCard',
+  mixins: [auth, device],
+  data() {
+    return {}
+  },
+  methods: {
+    logout() {
+      this.$auth.logout()
+      .then(() => this.$nuxt.$router.replace({ path: '/' }));
+    }
+  }
+}
+</script>
---
+++ components/cards/spotify-info-card.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card max-width="50rem" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          Spotify
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Status: linked
+        </v-list-item-title>
+        <v-list-item-subtitle>Your spotify account {{ user.integrations.spotify }} is working correctly.</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <!-- <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar> -->
+    </v-list-item>
+
+    <!-- <v-card-actions>
+      <v-btn class="ml-auto" text @click.stop="linkSpotify">
+        Link Spotify
+      </v-btn>
+    </v-card-actions> -->
+  </v-card>
+</template>
+
+<script>
+import auth from '~/mixins/auth'
+import device from '~/mixins/device'
+
+export default {
+  name: 'SpotifyInfoCard',
+  mixins: [auth, device],
+  data() {
+    return {}
+  },
+  methods: { }
+}
+</script>
