4cffe641577b22e140c92a6189e7008aecfd9a5a
elementh <elementh@users.noreply.github.com>
2022-10-10 10:00:42 +0000

    deploy: 1f23250268569c634fa78758b69acdda9336fb1a

---
+++ _nuxt/static/1665396040/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665396040",layout:"loading",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Flogin",config:{endpoints:{api:"https:\u002F\u002Fapi.spotihub.app"},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
